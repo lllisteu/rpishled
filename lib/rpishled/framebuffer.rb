2b2052817d2fadb632422f7bdb95b09ad64e1371
lllisteu <lllisteu@users.noreply.github.com>
2022-02-23 10:33:47 +0100

    Combine raw get/set

--- lib/rpishled/framebuffer.rb
+++ lib/rpishled/framebuffer.rb
@@ -1,15 +1,13 @@
 class Rpishled
 
-  # Reads the entire framebuffer. Returns a String of raw RGB565 data.
-  def raw
+  # Reads or writes the framebuffer.
+  def raw(rgb565=nil)
     rewind
-    read
-  end
-
-  # Writes raw RGB565 data directly to the framebuffer.
-  def raw=(rgb565)
-    rewind
-    write rgb565
+    if rgb565
+      write rgb565
+    else
+      read
+    end
   end
 
 end
